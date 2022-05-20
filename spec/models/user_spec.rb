7c34d215233024cece3a7b06ac42be8b3b896020
mammadu <mammadudiallo@gmail.com>
2022-05-20 12:42:55 -0500

    Add fields to user table

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it 'has a name column' do
+    expect(User.column_names).to include('name')
+  end
+
   it 'has a username column' do
     expect(User.column_names).to include('username')
+  end
+
+  it 'has a email column' do
+    expect(User.column_names).to include('email')
   end
 end
