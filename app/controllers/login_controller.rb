e25b706e0da51cb9019bd55456794d0656c00fac
mammadu <mammadudiallo@gmail.com>
2022-05-31 13:30:58 -0500

    login page shows message if login information is incorrect

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 class LoginController < ApplicationController
-
   def create
     email_address = params['address']
     if is_user(email_address)
       session[:current_user] = params['address']
       redirect_to user_index_url
     else
-      redirect_to root_path
+      flash[:alert] = "Login information incorrect"
+      redirect_to root_url
     end
-
   end
 
   private
@@ -18,5 +17,4 @@ class LoginController < ApplicationController
   def is_user(email_address)
     return User.find_by email: email_address
   end
-
 end
