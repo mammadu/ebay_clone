0b35e0f9905f958975521af6fa6ae44a62b01f82
mammadu <mammadudiallo@gmail.com>
2022-06-07 13:51:16 -0500

    can now create users in database using new form and user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,10 +6,20 @@ class UserController < ApplicationController
 
   def new
   end
-  
+
   def create
-    @user = User.new
-    # if @user.save
-    # end
+    @user = User.new(user_params)
+    if !@user.save
+      flash[:alert] = "please fill out all fields"
+      redirect_to new_user_path
+    end
   end
+
+  private 
+
+  def user_params
+    p params
+    params.permit(:name, :username, :email)
+  end
+
 end
