8a7d4eadefdeb3d8f66c3c11be0f57376ab3c510
mammadu <mammadudiallo@gmail.com>
2022-06-22 14:20:11 -0500

    rename test_row to test_user

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -4,36 +4,36 @@ require 'rails_helper'
 
 RSpec.describe LoginController do
   before(:each) do
-    @test_row = {
+    @test_user = {
       name: 'test_name',
       username: 'test_username',
       email: 'email@gmail.com',
       password: 'test_pass'
     }
-    @user = User.create(@test_row) # add item to database
+    @user = User.create(@test_user) # add item to database
   end
 
   # public method tests
   describe 'POST create' do
-    it 'creates a session' do
-      post :create, params: { email: @test_row[:email], password: @test_row[:password] }
-      expect(session[:current_user]).to eq @test_row[:email]
+    it 'creates a session given a matching email and password' do
+      post :create, params: { email: @test_user[:email], password: @test_user[:password] }
+      expect(session[:current_user]).to eq @test_user[:email]
     end
 
     it 'redirects to the user index page' do
-      response = post :create, params: { email: @test_row[:email], password: @test_row[:password] }
+      response = post :create, params: { email: @test_user[:email], password: @test_user[:password] }
       expect(response).to redirect_to(user_index_url)
     end
 
     it 'creates a flash warning if email information is incorrect' do
-      @test_row[:email] = nil
-      post :create, params: { email: @test_row[:email], password: @test_row[:password] }
+      @test_user[:email] = nil
+      post :create, params: { email: @test_user[:email], password: @test_user[:password] }
       expect(flash).to be_present
     end
 
     it 'creates a flash warning if password is incorrect' do
-      @test_row[:password] = nil
-      post :create, params: { email: @test_row[:email], password: @test_row[:password] }
+      @test_user[:password] = nil
+      post :create, params: { email: @test_user[:email], password: @test_user[:password] }
       expect(flash).to be_present
     end
   end
